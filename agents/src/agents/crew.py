9442ab4756b448f8903065ebacf653b00dc56bfb
Hady Ibrahim <hadyib007@gmail.com>
2025-01-11 18:28:36 -0500

    added agents and tasks, wip, doesnt give good output

--- agents/src/agents/crew.py
+++ agents/src/agents/crew.py
@@ -1,62 +1,122 @@
 from crewai import Agent, Crew, Process, Task
 from crewai.project import CrewBase, agent, crew, task
+from agents.tools.search import SearchTools
+from langchain_ollama import ChatOllama
+from langchain_cohere import ChatCohere
+from datetime import datetime
 
-# If you want to run a snippet of code before or after the crew starts, 
-# you can use the @before_kickoff and @after_kickoff decorators
-# https://docs.crewai.com/concepts/crews#example-crew-class-with-decorators
+# Llama3_2 = ChatOllama(model="llama3.2")
+
+llm = ChatCohere(temperature=0.3)
+now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+# Uncomment the following line to use an example of a custom tool
+# from Agents.tools.custom_tool import MyCustomTool
+
+# Check our tools documentations for more information on how to use them
+# from crewai_tools import SerperDevTool
+
 
 @CrewBase
-class Agents():
-	"""Agents crew"""
+class AgentsCrew:
+    """Agents crew"""
 
-	# Learn more about YAML configuration files here:
-	# Agents: https://docs.crewai.com/concepts/agents#yaml-configuration-recommended
-	# Tasks: https://docs.crewai.com/concepts/tasks#yaml-configuration-recommended
-	agents_config = 'config/agents.yaml'
-	tasks_config = 'config/tasks.yaml'
+    agents_config = "config/agents.yaml"
+    tasks_config = "config/tasks.yaml"
 
-	# If you would like to add tools to your agents, you can learn more about it here:
-	# https://docs.crewai.com/concepts/agents#agent-tools
-	@agent
-	def researcher(self) -> Agent:
-		return Agent(
-			config=self.agents_config['researcher'],
-			verbose=True
-		)
+    def manager(self) -> Agent:
+        return Agent(
+            config=self.agents_config["marketing_director"],
+            allow_delegation=True,
+            verbose=True,
+            max_iter=5,
+            llm=llm,
+        )
 
-	@agent
-	def reporting_analyst(self) -> Agent:
-		return Agent(
-			config=self.agents_config['reporting_analyst'],
-			verbose=True
-		)
+    @agent
+    def market_researcher(self) -> Agent:
+        return Agent(
+            config=self.agents_config["market_researcher"],
+            tools=[
+              SearchTools.open_page,
+            ],
+            verbose=True,
+            llm=llm,
+        )
 
-	# To learn more about structured task outputs, 
-	# task dependencies, and task callbacks, check out the documentation:
-	# https://docs.crewai.com/concepts/tasks#overview-of-a-task
-	@task
-	def research_task(self) -> Task:
-		return Task(
-			config=self.tasks_config['research_task'],
-		)
+    # @agent
+    # def content_strategist(self) -> Agent:
+    #     return Agent(
+    #         config=self.agents_config["content_strategist"],
+    #         verbose=True
+    #     )
 
-	@task
-	def reporting_task(self) -> Task:
-		return Task(
-			config=self.tasks_config['reporting_task'],
-			output_file='report.md'
-		)
+    @agent
+    def creative_designer(self) -> Agent:
+        return Agent(
+            config=self.agents_config["creative_designer"],
+            verbose=True,
+            allow_delegation=False,
+            llm=llm,
+        )
 
-	@crew
-	def crew(self) -> Crew:
-		"""Creates the Agents crew"""
-		# To learn how to add knowledge sources to your crew, check out the documentation:
-		# https://docs.crewai.com/concepts/knowledge#what-is-knowledge
+    @agent
+    def copywriter(self) -> Agent:
+        return Agent(
+            config=self.agents_config["copywriter"],
+            verbose=True,
+            llm=llm,
+        )
+        
 
-		return Crew(
-			agents=self.agents, # Automatically created by the @agent decorator
-			tasks=self.tasks, # Automatically created by the @task decorator
-			process=Process.sequential,
-			verbose=True,
-			# process=Process.hierarchical, # In case you wanna use that instead https://docs.crewai.com/how-to/Hierarchical/
-		)
+    @task
+    def market_research(self) -> Task:
+        return Task(
+            config=self.tasks_config["market_research"],
+            agent=self.market_researcher(),
+            output_file=f"src/agents/simulations/{now}/market_research.md",
+        )
+
+    # @task
+    # def content_strategy_task(self) -> Task:
+    #     return Task(
+    #         config=self.tasks_config["content_strategy"],
+    #         agent=self.content_strategist(),
+    #     )
+
+    @task
+    def creative_design_task(self) -> Task:
+        return Task(
+            config=self.tasks_config["creative_design"],
+            agent=self.creative_designer(),
+            output_file=f"src/agents/simulations/{now}/visual-content.md",
+        )
+
+    @task
+    def copywriting_task(self) -> Task:
+        return Task(
+            config=self.tasks_config["copywriting"],
+            agent=self.copywriter(),
+            output_file=f"src/agents/simulations/{now}/copywriting.json",
+        )
+    
+    @task
+    def move_character_task(self) -> Task:
+        return Task(
+            config=self.tasks_config["move_character"],
+            agent=self.manager(),
+            output_file=f"src/agents/simulations/{now}/movement.json",
+        )
+
+    @crew
+    def crew(self) -> Crew:
+        """Creates the Agents crew"""
+        return Crew(
+            agents=self.agents,  # Automatically created by the @agent decorator
+            tasks=self.tasks,  # Automatically created by the @task decorator
+            process=Process.hierarchical,
+            verbose=1,
+            # manager_llm=Llama3_2
+            manager_agent=self.manager()
+            # process=Process.hierarchical, # In case you wanna use that instead https://docs.crewai.com/how-to/Hierarchical/
+        )
